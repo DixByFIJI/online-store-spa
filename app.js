2df503ceb8209c66e45a9d436f2e2b8cabeb49d5
Vladislav <mandarineye@gmail.com>
2019-12-04 12:07:47 +0200

    Deploy

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 app.use(session({
-    session_secret: 'asdlnasdlasldkaksdjaslkdjakljdadskldjascaasdg',
+    secret: 'asdlnasdlasldkaksdjaslkdjakljdadskldjascaasdg',
     resave: false,
     saveUninitialized: false
 }));
