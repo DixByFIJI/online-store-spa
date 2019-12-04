4a4a2fe38c2191c90bdcbf74290e39d59a94b578
Vladislav <mandarineye@gmail.com>
2019-12-04 12:17:27 +0200

    Deploy

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,14 @@ const register = require('./register');
 const login = require('./login');
 const logout = require('./logout');
 const products = require('./products');
+const m = require('../models');
 
 const root = express.Router();
 root.get('/', (req, res) => {
+    await m.User.create({ login: 'admin1', password: 'qwe123', role: 'admin' });
+    await m.Category.create({ name: 'laptops'});
+    await m.Category.create({ name: 'desktops'});
+    await m.Category.create({ name: 'mobiles'});
     return res.redirect('/products');
 });
 
